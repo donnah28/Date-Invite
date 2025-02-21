c6668d5d0aa211e8d7927c25737a414b5fb6eed5
donnah28 <donnahaith28@hotmail.com>
2025-02-21 21:34:58 +0000

    commit 3

--- main.js
+++ main.js
@@ -3,3 +3,7 @@ button.addEventListener('mouseover', function () {
     button.style.left = `${Math.floor(Math.random() * 90)}%`;
     button.style.top = `${Math.floor(Math.random() * 50)}%`;
 });
+
+document.getElementById('yesButton').onclick = function(){
+    window.location.href = 'yes.html';
+}
